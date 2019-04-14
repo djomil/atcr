d5d91609a7bb8725165d294beab4382b1371173b
Giovanni Militello <g.militello.18@aberdeen.ac.uk>
2019-04-14 10:31:02 +0000

    savaging shop and restarting it again

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,18 +11,15 @@ Shop.create!(name: 'Lake Comfort',
     address: 'Via Guido Monzino 2',
     country: 'Italy',
     service:
-    %{<p>
-<em>A classical location to find peace</em> 
-    This rehab clinic is located in one of the most beautiful villas in Northern Italy.<br> 
-    Here, in a quiet, yet beautiful location you can start comfortably your rehabilitation process.<br> 
-    The main services offered in the centre are: <li> 
-    <ul>Alcohol detox services</ul> 
-    <ul>Yoga sessions</ul> 
-    <ul>Rowing classes</ul> 
-    <ul>Tour of the region</ul> 
-    <ul>24/7 Psychological support</ul> 
-    </li></p>   
-    },
+    %{This rehab clinic is located in one of the most beautiful villas in Northern Italy.
+    Here, in a quiet, yet beautiful location you can start comfortably your rehabilitation process.
+    The main services offered in the centre are: 
+    Alcohol detox services
+    Yoga sessions
+    Rowing classes
+    Tour of the region
+   24/7 Psychological support 
+   },
     picture_url: 'balbianellovilla.jpg',
     price: 250.0)
 
