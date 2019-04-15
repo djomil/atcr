1bf5f3b4216926cc083c751275e9a2f3d7722ff8
Giovanni Militello <g.militello.18@aberdeen.ac.uk>
2019-04-15 08:17:37 +0000

    validation done

--- app/models/rehab.rb
+++ app/models/rehab.rb
@@ -1,4 +1,10 @@
 class Rehab < ApplicationRecord
     validates :name, :address, :service, :picture_url, :price, presence: true  
+    validates :price, numericality: {greater_than_or_equal_to: 0.01}
+    validates :name, uniqueness: true
+    validates :picture_url, allow_blank: true, format: {
+with: %r{\.(gif|jpg|png)\Z}i,
+message: 'must be a URL for GIF, JPG or PNG image.'
+}
 end
 
