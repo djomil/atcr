71f14ed7ee369f9ed44e04b48aa8f07b00f0c760
Giovanni Militello <g.militello.18@aberdeen.ac.uk>
2019-04-17 08:07:39 +0000

    strange migration done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_16_095709) do
+ActiveRecord::Schema.define(version: 2019_04_17_075134) do
 
   create_table "alcoholconsumptions", force: :cascade do |t|
     t.string "entity"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2019_04_16_095709) do
     t.integer "cart_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "quantity", default: 1
     t.index ["cart_id"], name: "index_line_items_on_cart_id"
     t.index ["rehab_id"], name: "index_line_items_on_rehab_id"
   end
