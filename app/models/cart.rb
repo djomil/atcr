e214fafc6c2063cb505a261f86859fb15525fd60
Giovanni Militello <g.militello.18@aberdeen.ac.uk>
2019-04-17 08:11:21 +0000

    cart model extended

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,14 @@
 class Cart < ApplicationRecord
     has_many :line_items, dependent: :destroy
+    
+    def add_rehab(rehab)
+current_item = line_items.find_by(rehab_id: rehab.id)
+if current_item
+current_item.quantity += 1
+else
+current_item = line_items.build(rehab_id: rehab.id)
+end
+current_item
+end
+    
 end
